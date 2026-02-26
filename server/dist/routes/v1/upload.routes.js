bf50744810faca480b7f42db844ddd6f6d640a20
Shashank <shashank_official@hotmail.com>
2026-02-26 12:43:06 +0530

    refactor: removed dist folder

--- server/dist/routes/v1/upload.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const upload_controller_js_1 = require("../../controllers/upload.controller.js");
-const auth_middleware_js_1 = require("../../middlewares/auth.middleware.js");
-const router = (0, express_1.Router)();
-router.post("/", auth_middleware_js_1.requireAuth, upload_controller_js_1.generateUploadUrl);
-exports.default = router;
