c3c9feced6fbcd531233094d8abe2fe374d74901
Shashank <shashank_official@hotmail.com>
2026-03-10 21:56:55 +0530

    feat(express): add optional webhookTenantId to Request interface

--- server/express.d.ts
+++ server/express.d.ts
@@ -1,10 +1,12 @@
 import { Request } from "express";
+import { Types } from "mongoose";
 import { UserDocument } from "./src/types/models/user.model.types";
 
 declare global {
   namespace Express {
     interface Request {
       user: UserDocument;
+      webhookTenantId?: Types.ObjectId;
     }
   }
 }
