61bd1d238d6a435de8ec52c9ff7b52b6a2f107e5
Shashank <shashank_official@hotmail.com>
2026-03-10 12:54:15 +0530

    feat: add event listener for vite preload error to reload window

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+window.addEventListener('vite:preloadError', () => {
+  window.location.reload()
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
